05d9534f177c0a02411335e6e887df9d66f03007
Michu <michurodzyniak123@gmail.com>
2025-07-24 16:24:36 +0200

    Licence&Setup

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='RdLib',
-    version='0.1',
-    description='Simple Library to communicate with Rd03D Radar Via Serial on Raspberrypi',
+    version='0.9',
+    description='Simple Library to communicate with Rd03D and HLK-LD2450 Radar Via Serial on Raspberrypi',
     author='G20Michu',
     packages=find_packages(),
     install_requires=[
@@ -11,3 +11,4 @@ setup(
         'matplotlib'
     ],
 )
+url='https://github.com/G20Michu/RdLib.git'
